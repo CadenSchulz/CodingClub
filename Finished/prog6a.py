df7ce360c6763fa083f19aa5cb9003b4f76f1955
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2022-02-17 15:47:34 -0600

    main

---
+++ Finished/prog6a.py
@@ -0,0 +1,16 @@
+'''
+Created on Wed Feb 16 10:24:09 2022
+
+@author: C. Schulz
+
+Description: Counting up to 100
+
+Saved as prog6a.py
+'''
+
+    # Define Variable/Starting Point
+number = 1
+
+    # Create A Range To Print Each Number
+for number2 in range(0, 101):
+    print(number2)
