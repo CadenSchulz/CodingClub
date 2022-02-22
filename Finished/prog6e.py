7ecd47fed60f08a0616b28fa8efd1f4ee84d63a9
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2022-02-21 21:10:29 -0600

    main

--- Finished/prog6e.py
+++ Finished/prog6e.py
@@ -7,21 +7,26 @@ Description: Guessing game with numbers 1-10
 
 Saved as prog6e.py
 '''
-
+    # Imports
 import random
 
+    # Variables/Defining Range and Guess Count
 choiceBot = random.randrange(1,11)
 timesGuessed = 0
 
+    # Picking A Number 1-10
 while True:
     timesGuessed += 1
     yourChoice = int(input('Choose A Number 1-10: '))
+        # Seeing If Chosen Number Ranges From 1-10
     if yourChoice > 10 or yourChoice < 0:
         print('Not A Valid Number. Choose Again.')
         continue
+        # Output For Guessing The Correct Number/Showing Guess Count
     elif choiceBot == yourChoice:
         print(f'You guessed it! It only took you {timesGuessed} times to guess the correct number!')
         break
+        # Output For Guessing The Incorrect Number
     elif choiceBot != yourChoice:
         yourChoice = print('Sorry! Guess Again')
         continue
--- Finished/prog6f.py
+++ Finished/prog6f.py
@@ -7,26 +7,35 @@ Description: Flipping A Coin 1000 Times
 
 Saved as prog6f.py
 '''
+
+    # Imports
 import random
 
+    # List Of Heads Or Tails
 list1 = ['Heads', 'Tails']
 
+    # Variables/Times Flipped Per Heads Or Tails
 x = 1
 scoreHeads = 0
 scoreTails = 0
 
+    # Picking Heads Or Tails Randomly 1000 Times
 while x <= 1000:
     pick = random.choice(list1)
     x += 1
+        # Pick For Heads -- Adds 1 To How Many Times Heads Was Flipped
     if pick == 'Heads':
         scoreHeads += 1
 
+        # Pick For Tails -- Adds 1 To How Many Times Tails Was Flipped
     elif pick == 'Tails':
         scoreTails += 1
 
+        # Percentages For Heads/Tails Flipped
     percentageHeads = scoreHeads/1000 * 100
     percentageTails = scoreTails/1000 * 100
     
+    # Output For Percentages And How Many Times Heads or Tails Was Flipped Randomly
 print('\n')
 print("{:20s} {:20s}".format('HEADS', 'TAILS'))
 print("{:20s} {:20s}".format('-----', '-----'))
