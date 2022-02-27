c4099fc1ded262a289e910e4525f70fc1d1f1b7a
CadenSchulz <77269917+CadenSchulz@users.noreply.github.com>
2022-02-27 08:57:50 -0600

    main

---
+++ Finished/prog7f.py
@@ -0,0 +1,17 @@
+'''
+Created on Thurs Feb 24 5:42:31 2022
+
+@author: C. Schulz
+
+Description: Multiplication table ranging with 2 inputted numbers
+
+Saved as prog7f.py
+'''
+# num1 = int(input('First Number: '))
+# num2 = int(input('Second Number: '))
+# num3 = num2 + 1
+
+# range1 = range(num1, num3)
+# list1 = list(range1)
+
+# if list1
